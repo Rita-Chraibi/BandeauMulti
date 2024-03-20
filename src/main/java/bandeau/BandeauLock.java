c2e4a3a68e56841eeb028e12768f0e92f9e8241a
Rita-Chraibi <rita.chraibi.0@gmail.com>
2024-03-20 15:52:50 +0100

    modifications

---
+++ src/main/java/bandeau/BandeauLock.java
@@ -0,0 +1,15 @@
+package bandeau;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BandeauLock extends Bandeau {
+    private final Lock verrou = new ReentrantLock();
+    public void verrouiller() {
+        verrou.lock();
+    }
+    
+    public void deverrouiller() {
+        verrou.unlock();
+    }
+}
